bb888306a0e2fa517d79f0f429619ab7f1e0f92f
Haris Sikic <haris.sikic@gmail.com>
2023-11-14 23:31:21 +0100

    Import of models

--- mlp_graph_test.py
+++ mlp_graph_test.py
@@ -1,18 +1,40 @@
+import sys
+
+py_file_location = sys.path.insert(0, "/content/")
+
 from models.mlp import MLP
 from graphs.mlp_graph import MLPGraph
 from model_merger import ModelMerge
+from matching_functions import match_tensors_zipit
+
+from REPAIR_MTZ.models.mlp import MLP as REPAIR_MLP
+from REPAIR_MTZ.REPAIR import eval
+from REPAIR_MTZ.REPAIR.neural_align_diff import NeuralAlignDiff
 
 import torchvision.transforms as transforms
+
+from torch.utils.data import ConcatDataset
 
 import torch
 import torchvision
 
+def save_model(model, i):
+    sd = model.state_dict()
+    torch.save(sd, i)
+
+def load_model(model, i):
+    sd = torch.load(i)
+    model.load_state_dict(sd)
+
+
 if __name__ == "__main__":
-
     model1 = MLP(h=128, layers=5).eval()
-    graph1 = MLPGraph(model1, 5).graphify()  
-
     model2 = MLP(h=128, layers=5).eval()
+
+    load_model(model1, "./mlps2/fash_mnist_mlp_e50_l5_h128_v1.pt")
+    load_model(model2, "./mlps2/mnist_mlp_e50_l5_h128_v2.pt")
+
+    graph1 = MLPGraph(model1, 5).graphify()  
     graph2 = MLPGraph(model2, 5).graphify()  
 
     model3 = MLP(h=128, layers=5).eval()
@@ -22,19 +44,37 @@ if __name__ == "__main__":
             transforms.ToTensor(),
         ]
     )
-    dataset = torchvision.datasets.FashionMNIST(
+    FashionMNISTTrainSet = torchvision.datasets.FashionMNIST(
         root='./data', 
         train=True,
         download=True, 
         transform=transform
     )
-    dataloader = torch.utils.data.DataLoader(
-        dataset, 
+    FashionMNISTTrainLoader = torch.utils.data.DataLoader(
+        FashionMNISTTrainSet, 
+        batch_size=128,
+        shuffle=True, 
+        num_workers=8
+    )
+    MNISTTrainSet = torchvision.datasets.MNIST(
+        root='./data', 
+        train=True,
+        download=True, 
+        transform=transform
+    )
+    MNISTTrainLoader = torch.utils.data.DataLoader(
+        MNISTTrainSet, 
+        batch_size=128,
+        shuffle=True, 
+        num_workers=8
+    )
+    ConcatTrainLoader = torch.utils.data.DataLoader(
+        ConcatDataset((FashionMNISTTrainSet, MNISTTrainSet)), 
         batch_size=128,
         shuffle=True, 
         num_workers=8
     )
 
-    from matching_functions import match_tensors_identity, match_tensors_zipit
     merge = ModelMerge(graph1, graph2)
-    merge.transform(model3, dataloader, transform_fn=match_tensors_zipit)
+    merge.transform(model3, ConcatTrainLoader, transform_fn=match_tensors_zipit)
+     
