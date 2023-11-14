45fd7adeb06e6dadc24c2b7b7f35ed6494ef93c3
Haris Sikic <haris.sikic@gmail.com>
2023-11-14 22:04:03 +0100

    MLP models and graphs

---
+++ graphs/mlp_graph.py
@@ -0,0 +1,28 @@
+from graphs.base_graph import BIGGraph, NodeType
+
+class MLPGraph(BIGGraph):
+    def __init__(self, model, num_layers):
+        super().__init__(model)
+
+        self.num_layers = num_layers
+
+    def graphify(self):
+        input_node = self.create_node(node_type=NodeType.INPUT)
+
+        graph = []
+
+        graph.append('fc1')
+        graph.append(NodeType.PREFIX)
+
+        for i in range(self.num_layers * 2):
+            graph.append('layers.' + str(i))
+            if i % 2 != 0:
+                graph.append(NodeType.PREFIX)
+
+        graph.append('fc2')
+        graph.append(NodeType.OUTPUT)
+
+        self.add_nodes_from_sequence('', graph, input_node, sep='')
+
+        return self
+    
---
+++ models/mlp.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+import torch.nn.functional as F
+
+
+class MLP(nn.Module):
+    def __init__(self, h=128, layers=3):
+        super().__init__()
+
+        self.h          = h
+        self.num_layers = layers
+        self.fc1        = nn.Linear(28*28, h, bias=True)
+
+        mid_layers = []
+        for _ in range(layers):
+            mid_layers.extend([
+                nn.Linear(h, h, bias=True),
+                nn.ReLU(),
+            ])
+            
+        self.layers = nn.Sequential(*mid_layers)
+        self.fc2 = nn.Linear(h, 10)
+
+    def forward(self, x):
+        if x.size(1) == 3:
+            x = x.mean(1, keepdim=True)
+
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.layers(x)
+        x = self.fc2(x)
+
+        return x
