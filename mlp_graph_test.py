6dca70361e5f114d2d7ec11d5da936ecb8751753
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 01:06:34 +0100

    Fix merged model

--- mlp_graph_test.py
+++ mlp_graph_test.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
     merge = ModelMerge(graph1, graph2)
     merge.transform(model3, ConcatTrainLoader, transform_fn=match_tensors_zipit)
 
-    save_model(model3, "merged.pt")
+    save_model(merge, "merged.pt")
      
