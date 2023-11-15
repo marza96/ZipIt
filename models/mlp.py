157ef2b5f1989e7fe09a1967f64bc9a9ec4daa27
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 19:02:31 +0100

    Removing my submodule

--- models/mlp.py
+++ models/mlp.py
@@ -7,6 +7,8 @@ class MLP(nn.Module):
     def __init__(self, h=128, layers=3):
         super().__init__()
 
+        self.subnet     = Subnet
+
         self.h          = h
         self.num_layers = layers
         self.fc1        = nn.Linear(28*28, h, bias=True)
@@ -30,4 +32,21 @@ class MLP(nn.Module):
         x = self.layers(x)
         x = self.fc2(x)
 
+        return x
+    
+
+class Subnet(nn.Module):
+    def __init__(self, model, layer_i):
+        super().__init__()
+        self.model = model
+        self.layer_i = layer_i
+
+    def forward(self, x):
+        if x.size(1) == 3:
+            x = x.mean(1, keepdim=True)
+
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.model.fc1(x))
+        x = self.model.layers[:2 * self.layer_i](x)
+        
         return x
