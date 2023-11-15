c28e1a95d31cae8b06b6b0ba66cf85ddc0064df5
Haris Sikic <haris.sikic@gmail.com>
2023-11-15 20:57:42 +0100

    Refactor

--- run_mlp_merge_linux_cuda.sh
+++ run_mlp_merge_linux_cuda.sh
@@ -4,4 +4,4 @@ DEVICE="cuda"
 D0="FashionMNIST"
 D1="MNIST"
 
-python REPAIR/train.py --device $DEVICE --dataset0 $D0 --dataset1 $D1
+python mlp_merge.py --device $DEVICE --dataset0 $D0 --dataset1 $D1
